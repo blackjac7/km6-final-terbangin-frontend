afe28aedacad6ee9dffee401e37246bea33ec3c5
blackjac7 <febriansyah705@gmail.com>
2024-05-22 20:47:10 +0700

    initialize file

---
+++ src/components/Hit/index.jsx
@@ -0,0 +1,20 @@
+import { Highlight } from "react-instantsearch";
+import PropTypes from "prop-types";
+// Remove the unused import statement
+
+export const Hit = ({ hit }) => {
+    return (
+        <article style={{ textAlign: "left" }}>
+            <div className="hit-name">
+                <Highlight attribute="name" hit={hit} />
+            </div>
+            <div className="hit-city">
+                <Highlight attribute="city" hit={hit} />
+            </div>
+        </article>
+    );
+};
+
+Hit.propTypes = {
+    hit: PropTypes.object.isRequired,
+};
