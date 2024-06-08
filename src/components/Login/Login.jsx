36684c1386cf493a891bac6e1fb1704ef1a760b2
Muhammad Dwi Syamsurrijal <147080152+dwisyamsurrijal@users.noreply.github.com>
2024-06-08 21:45:24 +0700

    adding googleLogin component (#8)

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -4,6 +4,7 @@ import { useDispatch } from "react-redux";
 import { useNavigate, Link } from "react-router-dom";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import { login } from "../../redux/actions/auth";
+import GoogleLoginComponent from "../GoogleLogin/GoogleLogin";
 
 function Login() {
     const dispatch = useDispatch();
@@ -21,67 +22,81 @@ function Login() {
     };
 
     return (
-        <Form onSubmit={handleSubmit}>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email/No telepon</Form.Label>
-                <Form.Control
-                    type="email"
-                    placeholder="Contoh: john@gmail.com"
-                    value={email}
-                    onChange={(e) => {
-                        setEmail(e.target.value);
-                    }}
-                />
-            </Form.Group>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email/No telepon</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Contoh: john@gmail.com"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+        </Form.Group>
 
-            <div>
-                <Link
-                    to="/request-reset-password"
-                    className="float-end"
-                    style={{ paddingBottom: "13px", color: "#7126B5" }}
-                >
-                    Lupa kata sandi
-                </Link>
-            </div>
+        <div>
+          <Link
+            to="/request-reset-password"
+            className="float-end"
+            style={{ paddingBottom: "13px", color: "#7126B5" }}
+          >
+            Lupa kata sandi
+          </Link>
+        </div>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <div className="input-group">
-                <Form.Control
-                    type={showPassword ? "text" : "password"}
-                    placeholder="Masukkan password"
-                    value={password}
-                    onChange={(e) => {
-                        setPassword(e.target.value);
-                    }}
-                />
-                  <Button
-                        variant="outline-secondary"
-                        onClick={() => setShowPassword(!showPassword)}
-                        style={{ position: "absolute", top: "50%", transform: "translateY(-50%)", right: "2px", background: "none", border: "none", cursor: "pointer" }}
-                    >
-                        {showPassword ? <FaEyeSlash style={{ color: "#7126B5" }} /> : <FaEye style={{ color: "#7126B5" }} />}
-                    </Button>
-                </div>
-            </Form.Group>
-
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <div className="input-group">
+            <Form.Control
+              type={showPassword ? "text" : "password"}
+              placeholder="Masukkan password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
             <Button
-                variant="primary"
-                type="submit"
-                className="w-100 mb-3"
-                style={{ borderRadius: "12px", backgroundColor: "#7126B5" }}
-                disabled={loading}
+              variant="outline-secondary"
+              onClick={() => setShowPassword(!showPassword)}
+              style={{
+                position: "absolute",
+                top: "50%",
+                transform: "translateY(-50%)",
+                right: "2px",
+                background: "none",
+                border: "none",
+                cursor: "pointer",
+              }}
             >
-                {loading ? "Loading..." : "Masuk"}
+              {showPassword ? (
+                <FaEyeSlash style={{ color: "#7126B5" }} />
+              ) : (
+                <FaEye style={{ color: "#7126B5" }} />
+              )}
             </Button>
+          </div>
+        </Form.Group>
 
-            <div className="text-center mb-3">
-                Belum punya akun?{" "}
-                <Link to={"/register"} style={{ color: "#7126B5" }}>
-                    Daftar disini
-                </Link>
-            </div>
-        </Form>
+        <Button
+          variant="primary"
+          type="submit"
+          className="w-100 mb-3"
+          style={{ borderRadius: "12px", backgroundColor: "#7126B5" }}
+          disabled={loading}
+        >
+          {loading ? "Loading..." : "Masuk"}
+        </Button>
+        <p className="text-center text-black">Or</p>
+        <GoogleLoginComponent text={"Login with google"} />
+
+        <div className="text-center mb-3 mt-3">
+          Belum punya akun?{" "}
+          <Link to={"/register"} style={{ color: "#7126B5" }}>
+            Daftar disini
+          </Link>
+        </div>
+      </Form>
     );
 }
 
