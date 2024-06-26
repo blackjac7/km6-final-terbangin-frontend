be7076cc7f0ac3b8400ab9b604ca6b82c95a3b44
Raafi <92612605+Raafina@users.noreply.github.com>
2024-06-27 00:52:23 +0700

    Edit responsive formarea (#20)
    * find-ticket v1
    
    * continue propsing
    
    * change day selector border
    
    * solve merge
    
    * finish v1
    
    * test
    
    * change 23123
    
    * solve conflict
    
    * added few comment
    
    * just fetching some data without price and seat class logic
    
    * change accordion and styling
    
    * integrasi api ke fitur front end find ticket
    
    * test
    
    * edit style for accor trigger button, header accor trigger, and date selector
    
    * 1121
    
    * fix bug
    
    * pull
    
    * edit responsive
    
    ---------
    
    Co-authored-by: Raafina <raafinuradzani@gmail.com>
    Co-authored-by: Richard <richardaurelio42@gmail.com>

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -7,68 +7,68 @@ import CloseIcon from "@mui/icons-material/Close";
 import { useNavigate } from "react-router-dom";
 
 const loginPage = () => {
-    const navigate = useNavigate();
-    return (
-        <Container fluid className="vh-100">
-            <Row className="h-100">
-                <Col
-                    md={6}
-                    className="d-none d-md-block p-0 position-relative"
-                    style={{ backgroundColor: "#DEC9FF", position: "relative" }}
-                >
-                    <img
-                        src={image}
-                        alt="Login"
-                        className="position-absolute start-50 translate-middle-x"
-                        style={{
-                            maxWidth: "80%",
-                            maxHeight: "100%",
-                            transform: "translateX(-50%)",
-                            marginTop: "220px",
-                        }}
-                    />
-                    <img
-                        src={logo}
-                        alt="Logo"
-                        className="position-absolute"
-                        style={{
-                            maxWidth: "300px",
-                            left: "5px",
-                            zIndex: "1",
-                        }}
-                    />
-                </Col>
-                <Col
-                    md={6}
-                    className="d-flex align-items-center justify-content-center position-relative"
-                    style={{ paddingTop: "50px" }}
-                >
-                    <img
-                        src={logo2}
-                        alt="Logo2"
-                        className="d-block d-md-none position-absolute"
-                        style={{
-                            maxWidth: "170px",
-                            top: "70px",
-                            right: "30px",
-                            zIndex: "1",
-                        }}
-                    />
-                    <Button
-                        variant="link"
-                        className="text-decoration-none position-absolute top-0 end-0 mt-3 me-3"
-                        onClick={() => navigate("/")}
-                    >
-                        <CloseIcon style={{ color: "black" }} />
-                    </Button>
-                    <div className="w-75">
-                        <h2 className="mb-4">Masuk</h2>
-                        <Login />
-                    </div>
-                </Col>
-            </Row>
-        </Container>
-    );
+  const navigate = useNavigate();
+  return (
+    <Container fluid className="vh-100">
+      <Row className="h-100">
+        <Col
+          md={6}
+          className="d-none d-md-block p-0 position-relative"
+          style={{ backgroundColor: "#DEC9FF", position: "relative" }}
+        >
+          <img
+            src={image}
+            alt="Login"
+            className="position-absolute start-50 translate-middle-x"
+            style={{
+              maxWidth: "80%",
+              maxHeight: "100%",
+              transform: "translateX(-50%)",
+              marginTop: "220px",
+            }}
+          />
+          <img
+            src={logo}
+            alt="Logo"
+            className="position-absolute"
+            style={{
+              maxWidth: "300px",
+              left: "5px",
+              zIndex: "1",
+            }}
+          />
+        </Col>
+        <Col
+          md={6}
+          className="d-flex align-items-center justify-content-center position-relative"
+          style={{ paddingTop: "50px" }}
+        >
+          <img
+            src={logo2}
+            alt="Logo2"
+            className="d-block d-md-none position-absolute"
+            style={{
+              maxWidth: "170px",
+              top: "70px",
+              right: "30px",
+              zIndex: "1",
+            }}
+          />
+          <Button
+            variant="link"
+            className="text-decoration-none position-absolute top-0 end-0 mt-3 me-3"
+            onClick={() => navigate("/")}
+          >
+            <CloseIcon style={{ color: "black" }} />
+          </Button>
+          <div className="w-75">
+            <h2 className="mb-4">Masuk</h2>
+            <Login />
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default loginPage;
